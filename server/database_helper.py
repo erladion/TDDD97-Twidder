3d4d58a5450e836b53086cea0d27ab46e91f088a
erladion <johan.jansson94@gmail.com>
2017-02-07 15:04:29 +0100

    did stuff

--- server/database_helper.py
+++ server/database_helper.py
@@ -7,3 +7,52 @@ def create_database():
     with open('database.schema') as dbscript:
         cur.executescript(dbscript.read())
 
+def check_email(email):
+    cur.execute("SELECT rowid FROM userInformation WHERE email = ?", (email,))
+    data = cur.fetchone()
+    return data is not None
+
+def get_password(email):
+    cur.execute("SELECT password FROM userInformation WHERE email = ?", (email,))
+    data = cur.fetchone()
+    return data
+
+def create_user(firstname,lastname,email,gender,country,city,password):
+    cur.execute('insert into userInformation values (?,?,?,?,?,?,?)',(firstname,lastname,email,gender,country,city,password))
+    connection.commit()
+
+def insert_token(email,token):
+    cur.execute('insert into loggedInUsers values (?,?)',(email,token))
+    connection.commit()
+
+def get_token(email):
+    cur.execute("SELECT token FROM loggedInUsers WHERE email = ?", (email,))
+    data = cur.fetchone()
+    return data
+
+def remove_token(token):
+    cur.execute("delete from loggedInUsers where token = ?",(token,))
+    connection.commit()
+
+def get_email(token):
+    cur.execute("SELECT email FROM loggedInUsers WHERE token = ?", (token,))
+    data = cur.fetchone()
+    return data
+
+def change_password(email, password):
+    cur.execute("UPDATE userInformation SET password=? WHERE email=?", (password, email))
+    connection.commit()
+
+def get_user(email):
+    cur.execute("SELECT * FROM userInformation WHERE email = ?", (email,))
+    data = cur.fetchone()
+    return data
+
+def get_messages(email):
+    cur.execute("SELECT * FROM messages WHERE recieverEmail=?", (email,))
+    data = cur.fetchall()
+    return data
+
+def post_message(senderEmail, recieverEmail, message):
+    cur.execute("INSERT INTO messages VALUES (?, ?, ?)", (message, senderEmail, recieverEmail))
+    connection.commit()
--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
+# coding: utf8
 from flask import Flask
 from flask import request
 import database_helper
 import json
+import os
 
 app = Flask(__name__)
 
@@ -16,14 +18,113 @@ def signup():
     password = request.form['pass']
     if len(password) < 8:
         return json.dumps({'success': False, 'message': 'The password is too short'})
-    result = database_helper.create_user(firstname, familyname, email, gender, country, city, password)
-    if result:
-        return json.dumps({'success': True, 'message': 'All went well'})
+    if database_helper.check_email(email):
+        return json.dumps({'success': False, 'message': 'A user with that email already exists'})
+    database_helper.create_user(firstname, familyname, email, gender, country, city, password)
+    return json.dumps({'success': True, 'message': 'All went well'})
+
+@app.route("/signin", methods=["post"])
+def signin():
+    email = request.form["email"]
+    password = request.form["password"]
+    data = database_helper.get_password(email)
+    if data is None:
+        return json.dumps({'success': False, 'message': 'The email or password is incorrect'})
+
+    if data is not password:
+        return json.dumps({'success': False, 'message': 'The email or password is incorrect'})
+
+    token = database_helper.get_token(email)
+    if token is not None:
+        return json.dumps({'success': True, 'message': 'Already logged in', 'data': token})
     else:
-        return json.dumps({'success': False, 'message': 'User already exists'})
+        token = os.urandom(32)
+        database_helper.insert_token(email,token)
+        return json.dumps({'success': True, 'message': 'Successfully logged in', 'data': token})
 
+@app.route("/signout", methods=["post"])
+def signout():
+    database_helper.remove_token()
+    return json.dumps({'success': True, 'message': 'The user was logged out'})
 
+@app.route("/change_password", methods=["post"])
+def change_password():
+    oldPassword = request.form["oldpass"]
+    newPassword = request.form["newpass"]
+    token = request.form["token"]
 
+    if len(newPassword) < 8:
+        return json.dumps({'success': False, 'message': 'The password is too short'})
+
+    email = database_helper.get_email(token)
+    if email is None:
+        return json.dumps({'success': False, 'message': 'User is not logged in'})
+    else:
+        if database_helper.get_password(email) == oldPassword:
+            database_helper.change_password(email, newPassword)
+            return json.dumps({'success': True, 'message': 'Password was changed successfully'})
+        else:
+            return json.dumps({'success': False, 'message': 'Wrong password'})
+
+@app.route("/get_user_data_by_token", methods=["get"])
+def get_user_data_by_token():
+    token = request.form["token"]
+    email = database_helper.get_email(token)
+    if email is None:
+        return json.dumps({'success': False, 'message': 'User is not logged in'})
+    else:
+        data = database_helper.get_user(email)
+        return json.dumps({'success': True, 'message': 'Data retrieval successful', "data": data})
+
+@app.route("/get_user_data_by_email", methods=["get"])
+def get_user_data_by_email():
+    token = request.form["token"]
+    email = database_helper.get_email(token)
+    if email is None:
+        return json.dumps({'success': False, 'message': 'User is not logged in'})
+    else:
+        user_email = request.form["email"]
+        data = database_helper.get_user(user_email)
+        return json.dumps({'success': True, 'message': 'Data retrieval successful', "data": data})
+
+@app.route("/get_user_messages_by_token", methods=["get"])
+def get_user_messages_by_token():
+    token = request.form["token"]
+    email = database_helper.get_email(token)
+    if email is None:
+        return json.dumps({'success': False, 'message': 'User is not logged in'})
+    else:
+        data = database_helper.get_messages(email)
+        return json.dumps({'success': True, 'message': 'Data retrieval successful', "data": data})
+
+@app.route("/get_user_messages_by_email", methods=["get"])
+def get_user_messages_by_email():
+    token = request.form["token"]
+    email = database_helper.get_email(token)
+    if email is None:
+        return json.dumps({'success': False, 'message': 'User is not logged in'})
+    else:
+        user_email = request.form["email"]
+        data = database_helper.get_messages(user_email)
+        return json.dumps({'success': True, 'message': 'Data retrieval successful', "data": data})
+
+@app.route("/post_message", methods=["post"])
+def post_message():
+    token = request.form["token"]
+    message = request.form["message"]
+    email = request.form["email"]
+    if database_helper.check_email(email) is False:
+        return json.dumps({'success': False, 'message': 'User does not exist!'})
+    senderEmail = database_helper.get_email(token)
+
+    #
+    # CONTINUE HERE
+    #
+    #
+
+    if message is "":
+        return json.dumps({'success':False, 'message': 'Message cannot be empty'})
+    else:
 
 
 
